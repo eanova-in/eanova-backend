0e67d3bd41598213f4b6c69fa120cdd2d5eb7a0b
eanova-in <gamarulh1234@gmail.com>
2026-08-22 16:54:33 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -28,10 +28,15 @@ const otpStore = {};
 
 // Gmail Transporter
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
